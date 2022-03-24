a110a45e63b879e2cf40191825e02cbd3198ff2b
PhillTartari92 <phillipevianna@hotmail.fr>
2022-03-24 16:53:59 +0100

    add action update_user + add action softDeleteCommentary + double call route soft_delete_commentary

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,5 +17,17 @@ class ProfileController extends AbstractController
     {
        return $this->render('profile/show_profile.html.twig');
     } 
+
+    /**
+     * @Route("/profile/tous-mes-commentaires", name="show_user_commentaires", methods={"GET"})
+     */
+    public function showUserCommentaries(EntityManagerInterface $entityManager): Response
+    {
+       $commentaries = $entityManager->getRepository(Commentary::class)->findBy(['author' => $this->getUser()]);
+
+       return $this->render('profile/show_user_commentaries.html.twig', [
+           'commentaries' => $commentaries
+       ]);
+    }
 
 }
